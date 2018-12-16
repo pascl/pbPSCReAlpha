33b3116da875056d38e336286896a8f7de666a6c
pascl <pascl1983@gmail.com>
2018-12-16 18:26:07 +0100

    First version

---
+++ pbPSCReAlpha/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace pbPSCReAlpha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdGamesFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.tbFolderPath = new System.Windows.Forms.TextBox();
+            this.btCrowseGamesFolder = new System.Windows.Forms.Button();
+            this.btGo = new System.Windows.Forms.Button();
+            this.cbStartAt21 = new System.Windows.Forms.CheckBox();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbFolderPath
+            // 
+            this.tbFolderPath.Location = new System.Drawing.Point(12, 12);
+            this.tbFolderPath.Name = "tbFolderPath";
+            this.tbFolderPath.Size = new System.Drawing.Size(160, 20);
+            this.tbFolderPath.TabIndex = 0;
+            this.tbFolderPath.Text = "F:\\Games";
+            // 
+            // btCrowseGamesFolder
+            // 
+            this.btCrowseGamesFolder.Location = new System.Drawing.Point(178, 10);
+            this.btCrowseGamesFolder.Name = "btCrowseGamesFolder";
+            this.btCrowseGamesFolder.Size = new System.Drawing.Size(75, 23);
+            this.btCrowseGamesFolder.TabIndex = 1;
+            this.btCrowseGamesFolder.Text = "Browse...";
+            this.btCrowseGamesFolder.UseVisualStyleBackColor = true;
+            this.btCrowseGamesFolder.Click += new System.EventHandler(this.btCrowseGamesFolder_Click);
+            // 
+            // btGo
+            // 
+            this.btGo.Location = new System.Drawing.Point(178, 74);
+            this.btGo.Name = "btGo";
+            this.btGo.Size = new System.Drawing.Size(75, 23);
+            this.btGo.TabIndex = 2;
+            this.btGo.Text = "Do it!";
+            this.btGo.UseVisualStyleBackColor = true;
+            this.btGo.Click += new System.EventHandler(this.btGo_Click);
+            // 
+            // cbStartAt21
+            // 
+            this.cbStartAt21.AutoSize = true;
+            this.cbStartAt21.Checked = true;
+            this.cbStartAt21.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cbStartAt21.Location = new System.Drawing.Point(12, 38);
+            this.cbStartAt21.Name = "cbStartAt21";
+            this.cbStartAt21.Size = new System.Drawing.Size(241, 17);
+            this.cbStartAt21.TabIndex = 3;
+            this.cbStartAt21.Text = "Check to begin alphabetic order at 21st game";
+            this.cbStartAt21.UseVisualStyleBackColor = true;
+            // 
+            // tbLog
+            // 
+            this.tbLog.Location = new System.Drawing.Point(12, 103);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbLog.Size = new System.Drawing.Size(241, 185);
+            this.tbLog.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 300);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.cbStartAt21);
+            this.Controls.Add(this.btGo);
+            this.Controls.Add(this.btCrowseGamesFolder);
+            this.Controls.Add(this.tbFolderPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdGamesFolder;
+        private System.Windows.Forms.TextBox tbFolderPath;
+        private System.Windows.Forms.Button btCrowseGamesFolder;
+        private System.Windows.Forms.Button btGo;
+        private System.Windows.Forms.CheckBox cbStartAt21;
+        private System.Windows.Forms.TextBox tbLog;
+    }
+}
+
---
+++ pbPSCReAlpha/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pbPSCReAlpha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCrowseGamesFolder_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == fbdGamesFolder.ShowDialog())
+            {
+                String sFolderPath = fbdGamesFolder.SelectedPath;
+                tbFolderPath.Text = sFolderPath;
+            }
+        }
+
+        private void btGo_Click(object sender, EventArgs e)
+        {
+            /*List<String> testItems;
+            using (NaturalComparer comparer = new NaturalComparer())
+            {
+                testItems.Sort(comparer);
+            }*/
+            List<String> lsTitles = new List<string>();
+            Dictionary<String, int> dcTitleIndexes = new Dictionary<string, int>();
+            String sFolderPath = tbFolderPath.Text;
+            bool bStartAt21 = cbStartAt21.Checked;
+            try
+            {
+                var fileList = new DirectoryInfo(sFolderPath).GetFiles("*.ini", SearchOption.AllDirectories);
+                foreach(FileInfo fi in fileList)
+                {
+                    String s1 = fi.DirectoryName.Substring(sFolderPath.Length);
+                    if(s1.StartsWith("\\"))
+                    {
+                        s1 = s1.Substring(1);
+                    }
+                    //tbLog.AppendText(s1 + "\n");
+                    String[] s2 = s1.Split('\\');
+                    s1 = s2[0];
+                    int index = int.Parse(s1);
+                    String sTitle = "Undefined";
+                    if (((bStartAt21) && (index >= 21)) || (!bStartAt21))
+                    {
+                        using (StreamReader sr = new StreamReader(fi.FullName))
+                        {
+                            string s = "";
+                            while ((s = sr.ReadLine()) != null)
+                            {
+                                if (s.StartsWith("Title="))
+                                {
+                                    sTitle = s.Substring(6);
+                                    tbLog.AppendText("Found: " + s1 + " " + sTitle + "\n");
+                                    lsTitles.Add(sTitle);
+                                    dcTitleIndexes.Add(sTitle, index);
+                                }
+                            }
+                        }
+                        //tbLog.AppendText(fi.DirectoryName + " -- " + fi.Name + "\n");
+                    }
+                }
+                foreach(FileInfo fi in fileList)
+                {
+                    String s1 = fi.DirectoryName.Substring(sFolderPath.Length);
+                    if (s1.StartsWith("\\"))
+                    {
+                        s1 = s1.Substring(1);
+                    }
+                    //tbLog.AppendText(s1 + "\n");
+                    String[] s2 = s1.Split('\\');
+                    s1 = s2[0];
+                    int index = int.Parse(s1);
+                    if (((bStartAt21) && (index >= 21)) || (!bStartAt21))
+                    {
+                        Thread.Sleep(100);
+                        //tbLog.AppendText("o: " + sFolderPath + "\\" + index.ToString() + "\ts: " + sFolderPath + "\\" + (index + 1000).ToString() + "\n");
+                        Directory.Move(sFolderPath + "\\" + index.ToString(), sFolderPath + "\\" + (index + 1000).ToString());
+                    }
+                }
+                if(lsTitles.Count > 0)
+                {
+                    using (NaturalComparer comparer = new NaturalComparer())
+                    {
+                        lsTitles.Sort(comparer);
+                    }
+                    int newindex = 1;
+                    if (bStartAt21)
+                    {
+                        newindex = 21;
+                    }
+                    foreach(String s in lsTitles)
+                    {
+                        Thread.Sleep(100);
+                        int oldindex = dcTitleIndexes[s];
+                        Directory.Move(sFolderPath + "\\" + (oldindex + 1000).ToString(), sFolderPath + "\\" + newindex.ToString());
+                        tbLog.AppendText("New: " + newindex.ToString() + " " + s + "\n");
+                        newindex++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                tbLog.AppendText(ex.Message + "\n");
+            }
+        }
+    }
+}
