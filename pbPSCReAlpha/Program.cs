9678ef8fcc6d7010c4c4eab013d1fa9432be9135
pascl <pascl1983@gmail.com>
2019-10-20 01:00:10 +0200

    manage new xml files at app start

--- pbPSCReAlpha/Program.cs
+++ pbPSCReAlpha/Program.cs
@@ -21,6 +21,8 @@ namespace pbPSCReAlpha
             Application.SetCompatibleTextRenderingDefault(false);
 
             Dictionary<string, ClPS1Game> dcPs1Games = new Dictionary<string, ClPS1Game>();
+            Dictionary<string, ClTGDBGame> dcTgdbGames = new Dictionary<string, ClTGDBGame>();
+            Dictionary<int, string> dcTgdbPlatforms = new Dictionary<int, string>();
 
             try
             {
@@ -81,12 +83,127 @@ namespace pbPSCReAlpha
                 //slLogger.Fatal(ex.Message);
             }
 
+            try
+            {
+                using (XmlTextReader xmlreader = new XmlTextReader(Application.StartupPath + "\\" + "tgdbplatforms.xml"))
+                {
+                    int mykey = 0;
+                    String myvalue = String.Empty;
+                    while (xmlreader.Read())
+                    {
+                        switch (xmlreader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                // Console.WriteLine(xmlreader.Name);
+                                if ("platform" == xmlreader.Name)
+                                {
+                                    while (xmlreader.MoveToNextAttribute())
+                                    {
+                                        // Console.Write(" " + xmlreader.Name + "='" + xmlreader.Value + "'");
+                                        if ("id" == xmlreader.Name)
+                                        {
+                                            try
+                                            {
+                                                int.TryParse(xmlreader.Value, out mykey);
+                                            }
+                                            catch (Exception exept)
+                                            {
+                                                //
+                                                mykey = 0;
+                                            }
+                                        }
+                                    }
+                                }
+                            break;
+                            case XmlNodeType.Text:
+                                // Console.WriteLine(xmlreader.Value);
+                                myvalue = xmlreader.Value;
+                                if (!dcTgdbPlatforms.ContainsKey(mykey))
+                                {
+                                    dcTgdbPlatforms.Add(mykey, myvalue);
+                                    myvalue = String.Empty;
+                                    mykey = 0;
+                                }
+                                break;
+                            case XmlNodeType.EndElement:
+                                //
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //slLogger.Fatal(ex.Message);
+            }
+
+            try
+            {
+                using (XmlTextReader xmlreader = new XmlTextReader(Application.StartupPath + "\\" + "tgdbgames.xml"))
+                {
+                    String mylink = String.Empty;
+                    String myvalue = String.Empty;
+                    String myplatform = String.Empty;
+                    int iPlatform = 0;
+                    while (xmlreader.Read())
+                    {
+                        switch (xmlreader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                // Console.WriteLine(xmlreader.Name);
+                                if ("game" == xmlreader.Name)
+                                {
+                                    while (xmlreader.MoveToNextAttribute())
+                                    {
+                                        // Console.Write(" " + xmlreader.Name + "='" + xmlreader.Value + "'");
+                                        if ("platform" == xmlreader.Name)
+                                        {
+                                            myplatform = xmlreader.Value;
+                                        }
+                                        else if ("id" == xmlreader.Name)
+                                        {
+                                            mylink = xmlreader.Value;
+                                        }
+                                    }
+                                }
+                                break;
+                            case XmlNodeType.Text:
+                                // Console.WriteLine(xmlreader.Value);
+                                myvalue = xmlreader.Value;
+                                if (!dcTgdbGames.ContainsKey(mylink))
+                                {
+                                    if (int.TryParse(myplatform, out iPlatform))
+                                    {
+                                        if (dcTgdbPlatforms.ContainsKey(iPlatform))
+                                        {
+                                            dcTgdbGames.Add(mylink, new ClTGDBGame(myvalue, mylink, myplatform, dcTgdbPlatforms));
+                                            myvalue = String.Empty;
+                                            mylink = String.Empty;
+                                            myplatform = String.Empty;
+                                            iPlatform = 0;
+                                        }
+                                    }
+                                }
+                                break;
+                            case XmlNodeType.EndElement:
+                                //
+                                break;
+                        }
+                    }
+                    //slLogger.Debug("Found games in xml for search function: " + dcPs1Games.Count.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                //slLogger.Fatal(ex.Message);
+            }
+
             if ((args != null) && (args.Length == 1))
             {
                 if (args[0].Trim() == "-e")
                 {
                     bRunAForm = true;
-                    Application.Run(new Form23(Application.StartupPath, null, dcPs1Games, new ClVersionHelper("BleemSync v1.0.0", "", String.Empty, "..\\bleemsync\\etc\\bleemsync\\SYS\\databases", String.Empty)));
+                    Application.Run(new Form23(Application.StartupPath, null, dcPs1Games, dcTgdbGames, new ClVersionHelper("BleemSync v1.0.0", "", String.Empty, "..\\bleemsync\\etc\\bleemsync\\SYS\\databases", String.Empty)));
                 }
                 else
                 if (args[0].Trim() == "-b")
@@ -103,7 +220,7 @@ namespace pbPSCReAlpha
             }
             if (!bRunAForm)
             {
-                Application.Run(new Form1(dcPs1Games));
+                Application.Run(new Form1(dcPs1Games, dcTgdbGames));
             }
             
             
