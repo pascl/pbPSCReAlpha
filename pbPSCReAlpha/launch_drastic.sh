89b6be052770b3fea9a8ebe456f553eb0adf571d
pascl <pascl1983@gmail.com>
2019-10-24 13:33:00 +0200

    add missing modif for launch.sh edition

--- pbPSCReAlpha/launch_drastic.sh
+++ pbPSCReAlpha/launch_drastic.sh
@@ -3,5 +3,5 @@
 echo 2 > /data/power/disable
 chmod +x "/media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic/drastic"
 echo "launch_StockUI" > "/tmp/launchfilecommand"
-LD_PRELOAD=/media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic/drastic_sdl_remap.so /media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic/drastic "XXXXXYYYYY" > /media/logs/drastic.log 2>&1
+LD_PRELOAD=/media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic/drastic_sdl_remap.so /media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic/drastic "/var/volatile/launchtmp/XXXXXYYYYY" > /media/logs/drastic.log 2>&1
 echo 0 > /data/power/disable
