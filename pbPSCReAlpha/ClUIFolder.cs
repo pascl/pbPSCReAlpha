4a2fd016095c81b0ecfbeab8c1b04ec345b1e0d6
pascl <pascl1983@gmail.com>
2019-10-26 01:54:37 +0200

    New class for folders

---
+++ pbPSCReAlpha/ClUIFolder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pbPSCReAlpha
+{
+    public class ClUIFolder
+    {
+        List<int> _listGameIds;
+        String _title;
+
+        public ClUIFolder(string title, List<int> listGameIds)
+        {
+            _listGameIds = listGameIds;
+            _title = title;
+        }
+
+        public string Title { get => _title; set => _title = value; }
+        public List<int> ListGameIds { get => _listGameIds; set => _listGameIds = value; }
+    }
+}
