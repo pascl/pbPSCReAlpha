24a936d9650485911e062407fcf0f8e3d381c3c2
pascl <pascl1983@gmail.com>
2019-10-26 00:17:41 +0200

    launcher fix

--- pbPSCReAlpha/launch_drastic.sh
+++ pbPSCReAlpha/launch_drastic.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo 2 > /data/power/disable
-chmod +x "/media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic/drastic"
+cd "/media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic"
+chmod +x "drastic"
 echo "launch_StockUI" > "/tmp/launchfilecommand"
-LD_PRELOAD=/media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic/drastic_sdl_remap.so /media/bleemsync/etc/bleemsync/SUP/launchers/psc_drastic/drastic "/var/volatile/launchtmp/XXXXXYYYYY" > /media/logs/drastic.log 2>&1
+LD_PRELOAD=./drastic_sdl_remap.so ./drastic "/var/volatile/launchtmp/XXXXXYYYYY" > /media/logs/drastic.log 2>&1
 echo 0 > /data/power/disable
