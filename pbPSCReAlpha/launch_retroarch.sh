ee4d190186027f29abc4e548fd6d407476c2883d
pascl <pascl1983@gmail.com>
2020-01-25 11:22:27 +0100

    Add 2 lines in RA launch.sh template to be able to use compressed RA cores without any manual modifications

--- pbPSCReAlpha/launch_retroarch.sh
+++ pbPSCReAlpha/launch_retroarch.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo 2 > /data/power/disable
+source "/var/volatile/bleemsync.cfg"
+PATH="$PATH:$bleemsync_path/bin"
 mkdir -p "/tmp/ra_cache"
 chmod +x /media/bleemsync/opt/retroarch/retroarch
 HOME=/media/bleemsync/opt/retroarch /media/bleemsync/opt/retroarch/retroarch --config "/media/bleemsync/opt/retroarch/config/retroarch/retroarch.cfg" -L"/media/bleemsync/opt/retroarch/config/retroarch/cores/XXXXXYYYYY" "/var/volatile/launchtmp/YYYYYXXXXX" -v &> "/media/logs/retroarch.log"
